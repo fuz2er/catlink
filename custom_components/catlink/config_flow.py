1922e892db7e4a787e5871f8083bab1e359c79a4
Fuz2er <17903971+fuz2er@users.noreply.github.com>
2024-08-31 05:42:45 +0800

    optimise config_flow

--- custom_components/catlink/config_flow.py
+++ custom_components/catlink/config_flow.py
@@ -8,10 +8,10 @@ from .const import DOMAIN, CONF_API_BASE, CONF_PHONE, CONF_PHONE_IAC, CONF_PASSW
 
 ACCOUNT_SCHEMA = vol.Schema(
     {
-        vol.Optional(CONF_API_BASE, default=DEFAULT_API_BASE): str,
-        vol.Required(CONF_PHONE): str,
         vol.Required(CONF_PHONE_IAC, default='86'): str,
+        vol.Required(CONF_PHONE): str,
         vol.Required(CONF_PASSWORD): str,
+        vol.Optional(CONF_API_BASE, default=DEFAULT_API_BASE): str,
         vol.Optional(CONF_LANGUAGE, default='zh_CN'): str,
         # vol.Optional(CONF_SCAN_INTERVAL, default='00:01:00'): vol.All(cv.time_period_str),
     }
